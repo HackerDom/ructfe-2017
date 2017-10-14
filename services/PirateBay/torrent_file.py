07992462039693f2157d931b3ea9f63c937bddc5
melon <integrex@yandex.ru>
2017-10-15 02:29:25 +0500

    add trivial .torrent files maker

--- services/PirateBay/torrent_file.py
+++ services/PirateBay/torrent_file.py
@@ -50,6 +50,32 @@ def parse_torrent_file(data: bytes):
     return parse_dictionary(data)[0]
 
 
+def make_integer(num: int):
+    return INT_START + str(num).encode() + INT_END
+
+
+def make_string(source: bytes):
+    return str(len(source)).encode() + STRING_DELIMITER + source
+
+
+def make_list(obj_list: list):
+    return LIST_START + b''.join(MAKERS[type(obj)](obj) for obj in obj_list) + LICT_END
+
+
+def make_dictionary(obj_dict: dict):
+    sorted_keys = sorted(obj_dict.keys())
+    return DICT_START + b''.join(make_string(key) + MAKERS[type(obj_dict[key])](obj_dict[key])
+                                 for key in sorted_keys) + LICT_END
+
+
+MAKERS = {
+    list: make_list,
+    int: make_integer,
+    bytes: make_string,
+    dict: make_dictionary,
+}
+
+
 class TorrentFileInfo:
     def __init__(self, data):
         meta_dict = parse_torrent_file(data)
