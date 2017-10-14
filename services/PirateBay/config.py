43fddcab9f7a0e73cf583e0e6c99b528f44fe0cf
melon <integrex@yandex.ru>
2017-10-15 02:30:11 +0500

    add simple database MySQL-client

---
+++ services/PirateBay/config.py
@@ -0,0 +1,9 @@
+TORRENT_FILES_TABLE_NAME = "torrent_files"
+
+CONNECTION_PARAMS = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'passwd': 'kepler',
+    'db': 'pirate_bay',
+}
