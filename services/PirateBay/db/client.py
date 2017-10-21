d9e4630c2f7e1801923e6e3b891cb67ea75c91fd
melon <integrex@yandex.ru>
2017-10-21 16:13:54 +0500

    add filter query

--- services/PirateBay/db/client.py
+++ services/PirateBay/db/client.py
@@ -3,32 +3,36 @@ import pymysql
 from config import CONNECTION_PARAMS
 
 
-class Query:
-    def __init__(self, **params):
-        self.params = params
-        self.template = None
-
-    @property
-    def query(self):
-        return self.template.format(**self.params)
-
-
-class InsertQuery(Query):
-    def __init__(self, **params):
-        super().__init__(**params)
-        self.template = "INSERT INTO {tbl_name} ({field_names}) VALUES ({values});"
+class InsertQuery:
+    def __init__(self, tbl_name, field_names, values):
+        self.query = "INSERT INTO {tbl_name} ({field_names}) VALUES ({values});".format(
+            tbl_name=tbl_name,
+            field_names=', '.join(field_names),
+            values="'{}'".format("', '".join(values))
+        )
 
 
-class GetAllQuery(Query):
-    def __init__(self, **params):
-        super().__init__(**params)
-        self.template = "SELECT * FROM {tbl_name};"
+class GetAllQuery:
+    def __init__(self, tbl_name):
+        self.query = "SELECT * FROM {tbl_name};".format(
+            tbl_name=tbl_name,
+        )
 
 
-class CreateTableIfNotExistsQuery(Query):
-    def __init__(self, **params):
-        super().__init__(**params)
-        self.template = "CREATE TABLE IF NOT EXISTS {tbl_name}({fields});"
+class CreateTableIfNotExistsQuery:
+    def __init__(self, tbl_name, fields):
+        self.query = "CREATE TABLE IF NOT EXISTS {tbl_name}({fields});".format(
+            tbl_name=tbl_name,
+            fields=', '.join(' '.join(field) for field in fields),
+        )
+
+
+class FilterQuery:
+    def __init__(self, tbl_name, filter_fields):
+        self.query = "SELECT * FROM {tbl_name} WHERE {filter_fields};".format(
+            tbl_name=tbl_name,
+            filter_fields=' AND '.join("{}='{}'".format(*filter_field) for filter_field in filter_fields.items())
+        )
 
 
 class Singleton(type):
--- services/PirateBay/db/model.py
+++ services/PirateBay/db/model.py
@@ -1,6 +1,6 @@
 import pickle
 
-from db.client import GetAllQuery, InsertQuery, CreateTableIfNotExistsQuery, DBClient
+from db.client import GetAllQuery, InsertQuery, CreateTableIfNotExistsQuery, DBClient, FilterQuery
 from config import TORRENT_FILES_TABLE_NAME
 
 db_client = DBClient()
@@ -37,6 +37,11 @@ class IntField:
 
 
 class Model:
+    def __init__(self, field_values):
+        field_names = sorted(type(self).get_fields())
+        for field_name, field_value in zip(field_names, field_values):
+            setattr(self, field_name, field_value)
+
     @classmethod
     @cached_method
     def get_fields(cls):
@@ -46,7 +51,12 @@ class Model:
                     not callable(getattr(cls, field[0])))
 
     @classmethod
-    def validate(cls, fields):
+    def validate(cls, fields, check_required=True):
+        for field_name in fields:
+            if field_name not in cls.get_fields():
+                raise ValidationError("Undeclared field: {}".format(field_name))
+        if not check_required:
+            return
         for field_name, _ in cls.get_fields().items():
             if field_name not in fields:
                 raise ValidationError("Field {} is required".format(field_name))
@@ -58,9 +68,7 @@ class Model:
     @classmethod
     def create_table_if_not_exists(cls):
         table_name = cls.table_name()
-        fields = ", ".join(
-            field_name + " " + field_type.mysql_format for field_name, field_type in cls.get_fields().items()
-        )
+        fields = [(field_name, field_type.mysql_format) for field_name, field_type in cls.get_fields().items()]
         create_table_if_not_exists_query = CreateTableIfNotExistsQuery(
             tbl_name=table_name,
             fields=fields,
@@ -77,8 +85,8 @@ class Model:
         model_fields = cls.get_fields()
         sorted_field_names = sorted(model_fields.keys())
         field_values = [str(fields[field_name]) for field_name in sorted_field_names]
-        field_names_param = ", ".join(sorted_field_names)
-        field_values_param = "'{}'".format("', '".join(field_values))
+        field_names_param = sorted_field_names
+        field_values_param = field_values
         insert_query = InsertQuery(
             tbl_name=table_name,
             field_names=field_names_param,
@@ -93,7 +101,18 @@ class Model:
 
     @classmethod
     def all(cls):
+        cls.create_table_if_not_exists()
         get_all_query = GetAllQuery(tbl_name=cls.table_name()).query
         with db_client.connection.cursor() as cursor:
             cursor.execute(get_all_query)
             return list(cursor.fetchall())
+
+    @classmethod
+    def filter(cls, **fields):
+        cls.validate(fields, check_required=False)
+        cls.create_table_if_not_exists()
+        filter_query = FilterQuery(tbl_name=cls.table_name(), filter_fields=fields).query
+        with db_client.connection.cursor() as cursor:
+            cursor.execute(filter_query)
+            object_tuples = cursor.fetchall()
+            return [cls(object_tuple) for object_tuple in object_tuples]
