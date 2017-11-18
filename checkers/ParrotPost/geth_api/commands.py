00302cbd26616792836cf69c7838b67305ef4202
rx00 <it.tec@me.com>
2017-11-18 21:58:26 +0500

    + external js files support

--- checkers/ParrotPost/geth_api/commands.py
+++ checkers/ParrotPost/geth_api/commands.py
@@ -2,8 +2,9 @@ from geth_api.api import run_raw_command
 
 
 class GethController:
-    def __init__(self, ipc_path):
+    def __init__(self, ipc_path, scripts_path):
         self.ipc_addr = ipc_path
+        self.scripts_path = scripts_path
 
     def __run_geth_js_script(self, text_line):
         return run_raw_command(
@@ -16,5 +17,21 @@ class GethController:
     def stop_miner(self):
         return self.__run_geth_js_script("miner.stop()")
 
-    def start_miner(self):
-        return self.__run_geth_js_script("miner.start()")
+    def start_miner(self, threads_num=1):
+        return self.__run_geth_js_script("miner.start({})".format(threads_num))
+
+    def run_script(self, script_name):
+        command_result = run_raw_command(
+            "geth --jspath {} --exec loadScript(\"{}\") attach ipc:{}"
+            .format(self.scripts_path, script_name, self.ipc_addr)
+        )
+        if command_result.split("\n")[-1] == "true":
+            return "\n".join(command_result.split()[:-1])
+        else:
+            raise GethControllerException(
+                "Got error on script ({}) running ({})"
+                    .format(script_name, command_result))
+
+
+class GethControllerException(Exception):
+    pass
--- checkers/ParrotPost/main.py
+++ checkers/ParrotPost/main.py
@@ -2,8 +2,9 @@ from geth_api.commands import GethController
 
 
 PATH_TO_GETH_IPC = "/Users/ximik/ether_test_net/ructfe/geth.ipc"
-controller = GethController(PATH_TO_GETH_IPC)
+JS_SCRIPTS_PATH = "geth_scripts"
+controller = GethController(PATH_TO_GETH_IPC, JS_SCRIPTS_PATH)
 
-controller.start_miner()
-print(controller.get_current_block())
-controller.stop_miner()
+
+print(controller.get_current_block())  # via cli
+print(controller.run_script("get_current_block.js"))  # via external javascript file
