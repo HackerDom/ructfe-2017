e8eb5221fac85854de95e06371285b7023e640e1
melon <integrex@yandex.ru>
2017-11-28 04:25:29 +0500

    adjust config path

--- services/PirateBay/config.py
+++ services/PirateBay/config.py
@@ -2,6 +2,7 @@ import os
 
 TORRENT_FILES_TABLE_NAME = "torrent_files"
 
-DATABASE_DIR = '/home/melon/PycharmProjects/pirate-bay/db'
+DATABASE_DIR = os.path.join(os.getcwd(), 'db')
 DATABASE_NAME = 'pirate_bay'
 DATABASE_FULL_PATH = os.path.join(DATABASE_DIR, DATABASE_NAME)
+
