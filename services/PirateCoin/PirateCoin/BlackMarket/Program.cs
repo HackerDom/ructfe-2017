1579fd6b2a69430425d5d2531511517be6a26510
Konstantin Plotnikov <kost@hackerdom.ru>
2017-11-29 04:08:54 +0500

    PirateCoin BlackMarket subservice

---
+++ services/PirateCoin/PirateCoin/BlackMarket/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using PirateCoin;
+using PirateCoin.http;
+
+namespace BlackMarket
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			XmlConfigurator.Configure();
+			try
+			{
+				stateManager = new StateManager("state.json");
+				transactionChecker = new TransactionChecker(null, Settings.ParityRpcUrl);
+
+				var httpServer = new HttpServer(port);
+				httpServer
+					.AddHandler(HttpMethod.Get.ToString(), putFlagHttpPath, PutFlagCallback)
+					.AddHandler(HttpMethod.Get.ToString(), checkFlagHttpPath, CheckFlagCallback)
+					.AddHandler(HttpMethod.Get.ToString(), checkTransactionHttpPath, CheckTransactionCallback)
+					.AcceptLoopAsync(CancellationToken.None)
+					.Wait();
+			}
+			catch(Exception e)
+			{
+				log.Fatal("Unexpected App Error", e);
+			}
+		}
+
+		private static async Task PutFlagCallback(HttpListenerContext context)
+		{
+			var flag = context.Request.QueryString["flag"];
+			var contractAddr = context.Request.QueryString["contractAddr"];
+			var sumStr = context.Request.QueryString["sum"];
+
+			if(flag == null || contractAddr == null || !decimal.TryParse(sumStr, out var sum))
+			{
+				context.Close((int)HttpStatusCode.BadRequest);
+				return;
+			}
+
+			stateManager.Insert(new FlagData { contractAddr = contractAddr, flag = flag, sum = sum });
+		}
+
+		private static async Task CheckTransactionCallback(HttpListenerContext context)
+		{
+			var transaction = context.Request.QueryString["transaction"];
+			var contractAddr = context.Request.QueryString["contractAddr"];
+			if(transaction == null || contractAddr == null)
+			{
+				context.Close((int)HttpStatusCode.BadRequest);
+				return;
+			}
+
+			var flagsData = stateManager.FindByContractAddr(contractAddr);
+			if(flagsData == null)
+			{
+				context.Close((int)HttpStatusCode.NotFound);
+				return;
+			}
+
+			var flags = string.Join(",", flagsData.Select(data => data.flag));
+
+			var requestIp = context.Request.RemoteEndPoint.Address.ToString();
+			var hackerIp = flagsData.FirstOrDefault(data => data.hackerIp != null)?.hackerIp;
+			if(IsSameTeam(requestIp, hackerIp))
+			{
+				await context.WriteStringAsync(flags);
+				return;
+			}
+
+			if(hackerIp != null)
+			{
+				context.Close((int)HttpStatusCode.Gone);
+				return;
+			}
+
+			//NOTE race condition, but it's ok. just giving concurrnt hackers their flags
+			if(await transactionChecker.Check(transaction, contractAddr))
+			{
+				foreach(var flagData in flagsData)
+					flagData.hackerIp = requestIp;
+				await context.WriteStringAsync(flags);
+				return;
+			}
+
+			context.Close((int)HttpStatusCode.Unauthorized);
+		}
+
+		private static bool IsSameTeam(string ipA, string ipB)
+		{
+			if(ipA == ipB)
+				return true;
+			if(ipA == null || ipB == null)
+				return false;
+
+			return ipA.Substring(0, ipA.LastIndexOf(".")) == ipB.Substring(0, ipB.LastIndexOf("."));
+		}
+
+		private static async Task CheckFlagCallback(HttpListenerContext context)
+		{
+			var flag = context.Request.QueryString["flag"];
+			var contractAddr = context.Request.QueryString["contractAddr"];
+			if(flag == null || contractAddr == null)
+			{
+				context.Close((int)HttpStatusCode.BadRequest);
+				return;
+			}
+
+			var flagsData = stateManager.FindByContractAddr(contractAddr);
+			FlagData flagData;
+			if(flagsData == null || (flagData = flagsData.FirstOrDefault(fd => flag == fd.flag)) == null)
+			{
+				context.Close((int)HttpStatusCode.NotFound);
+				return;
+			}
+
+			if(flagData.hackerIp != null)
+				await context.WriteStringAsync(flagData.hackerIp);
+		}
+
+		const string putFlagHttpPath = "/putFlag";
+		const string checkFlagHttpPath = "/checkFlag";
+		const string checkTransactionHttpPath = "/checkTransaction";
+
+		private const int port = 14474;
+
+		private static StateManager stateManager;
+		private static TransactionChecker transactionChecker;
+
+		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+	}
+}
---
+++ services/PirateCoin/PirateCoin/BlackMarket/TransactionChecker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using Nethereum.Web3;
+
+namespace BlackMarket
+{
+	class TransactionChecker
+	{
+		public TransactionChecker(string abiFilepath, string parityRpcUrl)
+		{
+			this.abiFilepath = abiFilepath;
+			this.parityRpcUrl = parityRpcUrl;
+		}
+
+		private void ConnectToParityNode()
+		{
+			while(true)
+			{
+				try
+				{
+					web3 = new Web3(parityRpcUrl);
+					break;
+				}
+				catch(Exception e)
+				{
+					log.Info("Failed to connect to Parity node with specified coinbase password. Sleeping and retrying", e);
+					Thread.Sleep(5000);
+				}
+			}
+		}
+
+		public async Task<bool> Check(string transaction, string contractAddr)
+		{
+			var responseString = await web3.Client.SendRequestAsync<string>("trace_transaction", null, transaction);
+			Console.WriteLine(responseString);
+			return true;
+		}
+
+		private string abiFilepath;
+		private string parityRpcUrl;
+		private Web3 web3;
+
+		private static readonly ILog log = LogManager.GetLogger(typeof(TransactionChecker));
+	}
+}
