855d127b39776d56b235948cf055f74274adf117
Bogdan Melnik <teh.ld86@gmail.com>
2017-11-30 10:26:12 +0300

    Token and tests

--- services/powder/all_test.go
+++ services/powder/all_test.go
@@ -31,5 +31,35 @@ func TestHash(t *testing.T) {
 func TestToken(t *testing.T) {
     crypto := NewCrypto()
 
+    token := crypto.MakeToken("qwer")
+    login, err := crypto.LoginFromToken(token)
+
+    if err != nil {
+        t.Error("err != nil", err)
+    }
+
+    if login != "qwer" {
+        t.Error("login != \"qwer\"", login)
+    }
+
+    _, err = crypto.LoginFromToken("")
+    if err == nil {
+        t.Error("err == nil, 1")
+    }
+
+    _, err = crypto.LoginFromToken("qwer")
+    if err == nil {
+        t.Error("err == nil, 2")
+    }
+
+    token = crypto.MakeToken("qwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer")
+    login, err = crypto.LoginFromToken(token)
+    if err != nil {
+        t.Error("err != nil")
+    }
+
+    if login != "qwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer" {
+        t.Error("login != \"qwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer\"")
+    }
 
 }
--- services/powder/crypto.go
+++ services/powder/crypto.go
@@ -3,7 +3,14 @@ package main
 import (
     "math/rand"
     "crypto/md5"
+    "crypto/sha256"
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/hmac"
     "io"
+    "encoding/hex"
+    "time"
+    "errors"
 )
 
 type Crypto struct {
@@ -34,9 +41,58 @@ func (*Crypto) PasswordHash(salt string, password string) []byte {
 }
 
 func (crypto *Crypto) MakeToken(login string) string {
-    return login
+    block, err := aes.NewCipher(crypto.masterKey)
+    if err != nil {
+        panic(err)
+    }
+
+    rand.Seed(time.Now().Unix() / (60 * 60 * 24))
+
+    mac := hmac.New(sha256.New, crypto.masterKey)
+    loginBytes := []byte(login)
+    tokenBytes := make([]byte, mac.Size() + aes.BlockSize + len(loginBytes))
+
+    iv := tokenBytes[mac.Size():mac.Size() + aes.BlockSize]
+
+    rand.Read(iv)
+
+    stream := cipher.NewCTR(block, iv)
+    stream.XORKeyStream(tokenBytes[aes.BlockSize + mac.Size():], loginBytes)
+    mac.Write(tokenBytes[mac.Size():])
+
+    copy(tokenBytes[:mac.Size()], mac.Sum(nil)[:])
+
+    return hex.EncodeToString(tokenBytes)
 }
 
 func (crypto *Crypto) LoginFromToken(token string) (string, error) {
-    return token, nil
+    block, err := aes.NewCipher(crypto.masterKey)
+    if err != nil {
+        panic(err)
+    }
+
+    tokenBytes, err := hex.DecodeString(token)
+    if err != nil {
+        return "", err
+    }
+
+    mac := hmac.New(sha256.New, crypto.masterKey)
+    if len(tokenBytes) < aes.BlockSize + mac.Size() + 1 {
+        return "", errors.New("Wrong token")
+    }
+
+    macValue := tokenBytes[:mac.Size()]
+    mac.Write(tokenBytes[mac.Size():])
+
+    if !hmac.Equal(macValue, mac.Sum(nil)) {
+        return "", errors.New("Wrong mac")
+    }
+
+    iv := tokenBytes[mac.Size():mac.Size() + aes.BlockSize]
+    login := make([]byte, len(tokenBytes) - mac.Size() - aes.BlockSize)
+
+    stream := cipher.NewCTR(block, iv)
+    stream.XORKeyStream(login, tokenBytes[aes.BlockSize + mac.Size():])
+
+    return string(login), nil
 }
