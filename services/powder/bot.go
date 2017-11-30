8a813d1bf13eef68fd7cabbcce776027084462b5
Bogdan Melnik <teh.ld86@gmail.com>
2017-11-30 11:21:44 +0300

    Say Listen for bot

--- services/powder/bot.go
+++ services/powder/bot.go
@@ -6,41 +6,54 @@ import (
 )
 
 type Bot struct {
+    user *User
 }
 
-func NewBot() *Bot {
-    return &Bot{}
+func NewBot(user *User) *Bot {
+    return &Bot{user: user}
 }
 
 func SetupAutoReply(storage *Storage) {
     storage.IterateUsers(0, func (user *User) {
-        fmt.Printf("%v", user)
         if user.AutoReply {
             StartBot(user, storage)
         }
     })
 }
 
+func (bot *Bot) Listen(message *Message) {
+}
+
+func (bot *Bot) Say(message *Message) []*Message {
+    user := bot.user
+    messages := make([]*Message, 0)
+
+    if message.Author != user.Username {
+        botMessage := &Message{
+            From: user.Username,
+            To: message.Author,
+            Author: user.Username,
+            Message: fmt.Sprintf("Hi, I'm %s, nice to meet you!", user.Username),
+        }
+        messages = append(messages, botMessage)
+        botMessage = &Message{
+            From: message.Author,
+            To: user.Username,
+            Author: user.Username,
+            Message: fmt.Sprintf("Hi, I'm %s, nice to meet you!", user.Username),
+        }
+        messages = append(messages, botMessage)
+    }
+    return messages
+}
+
 func StartBot(user *User, storage *Storage) {
     go func() {
-        _ = NewBot()
+        bot := NewBot(user)
         for {
             lastMessage := storage.LastMessage(user.Username)
-            if lastMessage.Author != user.Username {
-                message := &Message{
-                    From: user.Username,
-                    To: lastMessage.Author,
-                    Author: user.Username,
-                    Message: fmt.Sprintf("Hi, I'm %s, nice to meet you!", user.Username),
-                }
-                storage.SaveMessage(message)
-
-                message = &Message{
-                    From: lastMessage.Author,
-                    To: user.Username,
-                    Author: user.Username,
-                    Message: fmt.Sprintf("Hi, I'm %s, nice to meet you!", user.Username),
-                }
+            bot.Listen(lastMessage)
+            for _, message := range bot.Say(lastMessage) {
                 storage.SaveMessage(message)
             }
             time.Sleep(5 * time.Second)
