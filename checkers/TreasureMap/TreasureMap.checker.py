13237f5ef3843707f957a144fc2470c5cba9f083
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2017-12-01 01:45:10 +0500

    TreasureMap: fix checker

--- checkers/TreasureMap/TreasureMap.checker.py
+++ checkers/TreasureMap/TreasureMap.checker.py
@@ -75,8 +75,7 @@ def is_between_str(l, r, p):
 	return l <= p <= r or l >= p >= r
 
 def is_between(l, r, p):
-	return l['x'] == p['x'] == r['x'] and is_between(l['y'], r['y'], p['y']) or l['y'] == p['y'] == r['y'] and is_between(l['x'], r['x'], p['x'])
-
+	return l['x'] == p['x'] == r['x'] and is_between_str(l['y'], r['y'], p['y']) or l['y'] == p['y'] == r['y'] and is_between_str(l['x'], r['x'], p['x'])
 
 async def check_path(username, sender, another, aname):
 	responses = []
@@ -116,13 +115,10 @@ async def handler_check(hostname):
 	username, password = await state.register()
 	tasks = []
 
-	await check_one(username, state, viewer, True)
-	await check_one(username, state, state, False)
-	await check_path(username, state, viewer, auser)
-#	tasks.append(asyncio.ensure_future(check_one(username, state, viewer, True)))
-#	tasks.append(asyncio.ensure_future(check_one(username, state, state, False)))
-#	tasks.append(asyncio.ensure_future(check_path(username, state, viewer, auser)))
-#	await asyncio.gather(*tasks)
+	tasks.append(asyncio.ensure_future(check_one(username, state, viewer, True)))
+	tasks.append(asyncio.ensure_future(check_one(username, state, state, False)))
+	tasks.append(asyncio.ensure_future(check_path(username, state, viewer, auser)))
+	await asyncio.gather(*tasks)
 
 	checker.ok()
 
