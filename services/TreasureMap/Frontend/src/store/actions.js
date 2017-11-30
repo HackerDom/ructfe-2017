a1370023f917bb028927eb14270d1a646b1706a9
Max Syrykh <slogger1994@gmail.com>
2017-12-01 02:04:16 +0500

    TreasureMap: frontend:add login form

--- services/TreasureMap/Frontend/src/store/actions.js
+++ services/TreasureMap/Frontend/src/store/actions.js
@@ -1,10 +1,15 @@
 import { createAction } from "redux-actions";
 import * as actions from "./actionsNames";
-import { fetchData as fetchDataService } from "../services/backend";
+import {
+  fetchData as fetchDataService,
+  login as loginHandler
+} from "../services/backend";
 
 const dataFetched = createAction(actions.DATA_FETCHED);
 export const createPoint = createAction(actions.CREATE_POINT);
 const dataFetchedFail = createAction(actions.DATA_FETCHED + actions.FAIL);
+export const loginOk = createAction(actions.LOGIN);
+const loginFail = createAction(actions.LOGIN + actions.FAIL);
 
 export const fetchData = () => {
   return async dispatch => {
@@ -12,6 +17,15 @@ export const fetchData = () => {
       dispatch(dataFetched(await fetchDataService()));
     } catch (e) {
       dispatch(dataFetchedFail());
+    }
+  };
+};
+
+export const login = (user, password) => {
+  return async dispatch => {
+    let res = await loginHandler(user, password);
+    if (res) {
+      dispatch(loginOk(user));
     }
   };
 };
