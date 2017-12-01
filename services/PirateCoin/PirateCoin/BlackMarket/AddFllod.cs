b0fef0933d9cd15d2472cb9e68ebdb16aac7cc6e
Konstantin Plotnikov <kost@hackerdom.ru>
2017-12-01 22:04:36 +0500

    Some PirateCoin logging imporvements

---
+++ services/PirateCoin/PirateCoin/BlackMarket/AddFllod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using PirateCoin.http;
+
+namespace BlackMarket
+{
+	public static class AntiFlood
+	{
+		public static long TotalSeconds(this DateTime dt)
+		{
+			return dt.Ticks / 10000000;
+		}
+
+		public static void CheckFlood(string key, int duration = CheckIntervalDurationSeconds, int count = MaxCountInInterval)
+		{
+			var checkTime = DateTime.UtcNow.TotalSeconds() / duration;
+			Dict.AddOrUpdate(key, new FloodCheck { Time = checkTime, Count = 1 }, (l, check) =>
+			{
+				lock(check)
+				{
+					if(check.Time == checkTime)
+					{
+						if(++check.Count > count)
+							throw new HttpException(403, $"Too fast, wait {DateTime.UtcNow.TotalSeconds() - checkTime * duration + 1} sec pls");
+					}
+					else
+					{
+						check.Time = checkTime;
+						check.Count = 1;
+					}
+					return check;
+				}
+			});
+		}
+
+		private class FloodCheck
+		{
+			public long Time;
+			public int Count;
+		}
+
+		private const int CheckIntervalDurationSeconds = 10;
+		private const int MaxCountInInterval = 10;
+
+		private static readonly ConcurrentDictionary<string, FloodCheck> Dict = new ConcurrentDictionary<string, FloodCheck>(StringComparer.InvariantCultureIgnoreCase);
+	}
+}
--- services/PirateCoin/PirateCoin/BlackMarket/Program.cs
+++ services/PirateCoin/PirateCoin/BlackMarket/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -40,89 +41,89 @@ namespace BlackMarket
 			var contractAddr = context.Request.QueryString["contractAddr"]?.ToLowerInvariant();
 			var sumStr = context.Request.QueryString["sum"];
 
-			if(flag == null || contractAddr == null || !decimal.TryParse(sumStr, out var sum))
-			{
-				context.Close((int)HttpStatusCode.BadRequest);
-				return;
-			}
+			if(flag == null || contractAddr == null || !decimal.TryParse(sumStr, NumberStyles.Any, CultureInfo.InvariantCulture, out var sum))
+				throw new HttpException((int)HttpStatusCode.BadRequest, "expected params 'flag', 'contractAddr', 'sum'");
 
 			stateManager.Insert(new FlagData { contractAddr = contractAddr, flag = flag, sum = sum });
+
+			await context.WriteStringAsync("Done");
 		}
 
+		
 		private static async Task CheckTransactionCallback(HttpListenerContext context)
 		{
+			var requestIp = context.Request.RemoteEndPoint.Address.ToString();
+			var team = GetNormalizedTeamSubnetFromIp(requestIp);
+
+			AntiFlood.CheckFlood(team, 60, 768);
+
 			var transaction = context.Request.QueryString["transaction"];
 			var contractAddr = context.Request.QueryString["contractAddr"]?.ToLowerInvariant();
 			if(transaction == null || contractAddr == null)
-			{
-				context.Close((int)HttpStatusCode.BadRequest);
-				return;
-			}
+				throw new HttpException((int)HttpStatusCode.BadRequest, "expected params 'transaction', 'contractAddr'");
 
-			var flagsData = stateManager.FindByContractAddr(contractAddr);
-			if(flagsData == null)
-			{
-				context.Close((int)HttpStatusCode.NotFound);
-				return;
-			}
+			var flags = stateManager.FindByContractAddr(contractAddr);
+			if(flags == null)
+				throw new HttpException((int) HttpStatusCode.NotFound, $"don't have flags for contract '{contractAddr}'");
 
-			var flags = string.Join(",", flagsData.Select(data => data.flag));
+			var flagsString = string.Join(",", flags.Select(data => data.flag));
 
-			var requestIp = context.Request.RemoteEndPoint.Address.ToString();
-			var hackerIp = flagsData.FirstOrDefault(data => data.hackerIp != null)?.hackerIp;
+			var hackerIp = flags.FirstOrDefault(data => data.hackerIp != null)?.hackerIp;
 			if(hackerIp != null)
 			{
 				if(IsSameTeam(requestIp, hackerIp))
-					await context.WriteStringAsync(flags);
+					await context.WriteStringAsync(flagsString);
 				else
-					context.Close((int)HttpStatusCode.Gone);
+					throw new HttpException((int)HttpStatusCode.Gone, $"flag(s) from this contract are already stolen and not by your team, your ip: {requestIp}");
 				return;
 			}
 
 			var isContactBalanceHacked = await transactionChecker.CheckContractBalanceIsHacked(contractAddr);
 			if(!isContactBalanceHacked)
-			{
-				context.Close((int)HttpStatusCode.Unauthorized);
-				return;
-			}
+				throw new HttpException((int) HttpStatusCode.Unauthorized,
+					"contract does not look being hacked (total balance on all deposits seems equal to contract balance)");
 
-			//NOTE race condition, but it's ok. just giving concurrent hackers their flags
-			var hackerContractAddr = await transactionChecker.CheckTransactionAndFindHackerContractAddr(transaction, contractAddr, flagsData.First().sum);
+			var hackerContractAddr = await transactionChecker.CheckTransactionAndFindHackerContractAddr(transaction, contractAddr, flags.First().sum);
 			if(hackerContractAddr ==  null)
-			{
-				context.Close((int)HttpStatusCode.Unauthorized);
-				return;
-			}
+				throw new HttpException((int)HttpStatusCode.Unauthorized,
+					"contract does not look being hacked (your transaction should show stealing coins to single recipient addr and amount not less than we deposited)");
 
 			var hackerContractOwnerIp = await transactionChecker.FindHackerContractOwnerIp(hackerContractAddr);
 			if(hackerContractOwnerIp == null || !IsSameTeam(requestIp, hackerContractOwnerIp))
 			{
-				context.Close((int)HttpStatusCode.Unauthorized);
-				return;
+				throw new HttpException((int)HttpStatusCode.Unauthorized,
+					$"contract looks hacked by somebody else ({hackerContractOwnerIp}), not by your team, your ip: {requestIp}");
 			}
 
-
-			foreach(var flagData in flagsData)
+			foreach(var flagData in flags)
 			{
 				flagData.hackerIp = requestIp;
 				stateManager.Insert(flagData);
 			}
-			await context.WriteStringAsync(flags);
+			await context.WriteStringAsync(flagsString);
 		}
 
 		const string bankContractAbiFilepath = "contracts/bank.abi.json";
-		const string bankAttackerContractAbiFilepath = "contracts/bankAttacker.abi.json";
+		const string bankAttackerContractAbiFilepath = "contracts/bankattacker.abi.json";
 
-		
+		private static string GetNormalizedTeamSubnetFromIp(string ip)
+		{
+			if(ip == null)
+				return null;
+
+			int idx;
+			if((idx = ip.LastIndexOf(".", StringComparison.Ordinal)) == -1)
+				return null;
+
+			var subnet = ip.Substring(0, idx);
+			return subnet.Replace("10.8", "10.6");//FUCK YEAH, ugly shit to save teams, posting requests from their router
+		}
 
 		private static bool IsSameTeam(string ipA, string ipB)
 		{
 			if(ipA == ipB)
 				return true;
-			if(ipA == null || ipB == null)
-				return false;
-
-			return ipA.Substring(0, ipA.LastIndexOf(".")) == ipB.Substring(0, ipB.LastIndexOf("."));
+			return GetNormalizedTeamSubnetFromIp(ipA).Equals(GetNormalizedTeamSubnetFromIp(ipB), StringComparison.Ordinal);
 		}
 
 		private static async Task CheckFlagCallback(HttpListenerContext context)
@@ -130,25 +131,19 @@ namespace BlackMarket
 			var flag = context.Request.QueryString["flag"];
 			var contractAddr = context.Request.QueryString["contractAddr"]?.ToLowerInvariant();
 			if(flag == null || contractAddr == null)
-			{
-				context.Close((int)HttpStatusCode.BadRequest);
-				return;
-			}
+				throw new HttpException((int)HttpStatusCode.BadRequest, "expected params 'flag', 'contractAddr'");
 
 			var flagsData = stateManager.FindByContractAddr(contractAddr);
 			FlagData flagData;
 			if(flagsData == null || (flagData = flagsData.FirstOrDefault(fd => flag == fd.flag)) == null)
-			{
-				context.Close((int)HttpStatusCode.NotFound);
-				return;
-			}
+				throw new HttpException((int)HttpStatusCode.NotFound, $"can't find flag for provided params flag '{flag}', contractAddr '{contractAddr}'");
 
 			if(flagData.hackerIp != null)
-				await context.WriteStringAsync(flagData.hackerIp);
+				await context.WriteStringAsync("stolen");
 		}
 
-		const string putFlagHttpPath = "/putFlag";
-		const string checkFlagHttpPath = "/checkFlag";
+		const string putFlagHttpPath = "/putFlag_C6EDEE7179BD4E2887A5887901F23060";
+		const string checkFlagHttpPath = "/checkFlag_C6EDEE7179BD4E2887A5887901F23060";
 		const string checkTransactionHttpPath = "/checkTransaction";
 
 		private const int port = 14474;
