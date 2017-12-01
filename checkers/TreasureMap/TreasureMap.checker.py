afa55c34349c52567118b7d68937ae28dd48b8b5
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2017-12-01 23:53:35 +0500

    TreasureMap: another one

--- checkers/TreasureMap/TreasureMap.checker.py
+++ checkers/TreasureMap/TreasureMap.checker.py
@@ -117,7 +117,7 @@ async def handler_check(hostname):
 
 async def handler_get_1(hostname, id, flag):
 	id = json.loads(id)
-	p = get_point_with_flag(hostname, id['username'], id['password'], id['id'])
+	p = await get_point_with_flag(hostname, id['username'], id['password'], id['id'])
 	if p['message'] != flag:
 		checker.corrupt(message="Bad flag: expected {}, found {}".format(flag, p['message']))
 	checker.ok()
@@ -131,7 +131,7 @@ async def handler_put_1(hostname, id, flag):
 
 async def handler_get_2(hostname, id, flag):
 	id = json.loads(id)
-	p = get_point_with_flag(hostname, id['username'], id['password'], id['id'])
+	p = await get_point_with_flag(hostname, id['username'], id['password'], id['id'])
 	if p['x'] + p['y'] != flag:
 		checker.corrupt(message="Bad flag: expected {}, found {}".format(flag, p['message']))
 	checker.ok()
