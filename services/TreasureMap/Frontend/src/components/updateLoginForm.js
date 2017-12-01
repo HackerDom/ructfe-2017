db90c9ac7e8c2fac9a6f84c06243d9dde1f94cfe
Max Syrykh <slogger1994@gmail.com>
2017-12-01 23:28:51 +0500

    TreasureMap: frontend: catch 403 code

--- services/TreasureMap/Frontend/src/components/updateLoginForm.js
+++ services/TreasureMap/Frontend/src/components/updateLoginForm.js
@@ -7,6 +7,8 @@ let withHandler = false;
 
 export default user => {
   let wrapper = document.querySelector(".userForm");
+  let userWrapper = wrapper.querySelector(".user");
+  let loginFormWrapper = wrapper.querySelector(".login");
   if (!withHandler) {
     wrapper.addEventListener("submit", async e => {
       const { user, password } = serialize(e.target, {
@@ -21,8 +23,12 @@ export default user => {
   }
 
   if (user) {
-    wrapper.innerText = `Hi, ${user}`;
+    loginFormWrapper.hidden = true;
+    userWrapper.hidden = false;
+    userWrapper.textContent = `Hi, ${user}`;
   } else {
+    loginFormWrapper.hidden = false;
+    userWrapper.hidden = true;
     return wrapper;
   }
 };
