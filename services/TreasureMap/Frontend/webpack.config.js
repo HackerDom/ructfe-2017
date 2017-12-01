4febcd7ef50707bb5ea11b35ba6ec77dfd88681c
Max Syrykh <slogger1994@gmail.com>
2017-12-01 23:32:50 +0500

    TreasureMap: frontend: webpack.DefinePlugin

--- services/TreasureMap/Frontend/webpack.config.js
+++ services/TreasureMap/Frontend/webpack.config.js
@@ -26,6 +26,11 @@ let config = {
     new HtmlWebpackPlugin({
       title: "Treasure map",
       template: "src/index.html"
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development")
+      }
     })
   ]
 };
